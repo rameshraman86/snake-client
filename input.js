ce2fdff1d4bd31dcfba025ad65d75846032da701
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-09 08:25:05 -0400

    updated play.js

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
 
+// Stores the active TCP connection object.
+let conn;
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(connection) {
+  conn = connection;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -16,6 +20,20 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (key === 'w') {
+    conn.write("Move: up");
+  }
+  if (key === 'a') {
+    conn.write("Move: left");
+  }
+  if (key === 's') {
+    conn.write("Move: down");
+  }
+  if (key === 'd') {
+    conn.write("Move: right");
+  }
+
 };
 
 module.exports = { setupInput };
